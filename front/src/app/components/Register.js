8d1feebf6a48b679ed6c961fe5a083f6d62a9701
aymen4577 <77078351+aymen4577@users.noreply.github.com>
2023-05-19 12:12:45 +0100

    Update Register.js

--- front/src/app/components/Register.js
+++ front/src/app/components/Register.js
@@ -63,7 +63,7 @@ const verificationCode = Date.now()
       name: username,
         email:email,
         code:verificationCode,
-        lien:"http://localhost:8080/EmailVerification",
+        lien:"http://20.204.236.214:8081/EmailVerification",
         }
         UserService.verfieremail(dataemail).then(
           (res)=>{
